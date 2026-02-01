c302ebc2c09bf98a8f63cdc9d414f90cd2b777c6
Azarov204 <adamazarov99@gmail.com>
2026-02-01 16:14:00 -0600

    Change config files to hopefully fix github pages

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
-import autoprefixer from "autoprefixer";
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "/starbuck-curling/",
   plugins: [
     react(),
-    tailwindcss(),
-    autoprefixer()
+    tailwindcss()
   ],
 });
