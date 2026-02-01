7c37e96c121a4eeda7e43e99d958ea11f005d07c
Azarov204 <adamazarov99@gmail.com>
2026-02-01 15:36:51 -0600

    Add ScrollToTop component for all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
@@ -6,19 +6,23 @@ import About from "./pages/About/About";
 import Leagues from "./pages/Leagues/Leagues";
 import Merch from "./pages/Merch/Merch";
 import NotFound from "./pages/NotFound/NotFound";
+import Footer from "./components/Footer/Footer.jsx";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop.jsx";
 
 const App = () => {
   return (
-    <>
-      <Header />
+    <div className="min-h-screen bg-website-bg text-dark-text overflow-hidden">
+      <Header/>
+      <ScrollToTop/>
       <Routes>
-        <Route path="/starbuck-curling/" element={<Home />} />
-        <Route path="/starbuck-curling/about" element={<About />} />
-        <Route path="/starbuck-curling/leagues" element={<Leagues />} />
-        <Route path="/starbuck-curling/merch" element={<Merch />} />
-        <Route path="/starbuck-curling/*" element={<NotFound />} />
+        <Route path="/starbuck-curling/" element={<Home/>}/>
+        <Route path="/starbuck-curling/about" element={<About/>}/>
+        <Route path="/starbuck-curling/leagues" element={<Leagues/>}/>
+        <Route path="/starbuck-curling/merch" element={<Merch/>}/>
+        <Route path="/starbuck-curling/*" element={<NotFound/>}/>
       </Routes>
-    </>
+      <Footer/>
+    </div>
   );
 };
 
