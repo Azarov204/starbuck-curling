92b8b529fcd729e8cecbbeb81eb51e5abb278ece
Azarov204 <adamazarov99@gmail.com>
2026-09-24 14:29:11 -0500

    Update NotFound page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,14 +1,23 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import Contact from "../../components/Contact/Contact.jsx";
 
 const NotFound = () => {
   return (
-    <div className="container">
-      <h1>404 - Page Not Found</h1>
-      <p>Sorry, the page you are looking for does not exist.</p>
-      <Link to="/starbuck-curling/">
-        <button>Go to Home Page</button>
-      </Link>
-    </div>
+    <>
+      <div className="mx-auto max-w-7xl pt-30 grow">
+        <h1>Seems like you're lost!</h1>
+        <h6 className="pt-6">
+          {"Click "}
+          <Link to="/">
+            <button className="cursor-pointer text-starbuck hover:text-header-light font-bold">
+              HERE
+            </button>
+          </Link>
+          {" to go back to the home page."}
+        </h6>
+      </div>
+      <Contact />
+    </>
   );
 };
 
